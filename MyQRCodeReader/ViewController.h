f89fb01193aeb1d254c751ffde9c6b3b9de2876e
Shanshan ZHAO <chezzhaoss@gmail.com>
2014-05-26 14:58:18 +0200

    Initial Commit

---
+++ MyQRCodeReader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyQRCodeReader
+//
+//  Created by Shanshan ZHAO on 26/05/14.
+//  Copyright (c) 2014 Shanshan ZHAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MyQRCodeReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyQRCodeReader
+//
+//  Created by Shanshan ZHAO on 26/05/14.
+//  Copyright (c) 2014 Shanshan ZHAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
